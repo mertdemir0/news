1a68f6bba858929960a59ed48b263a645a1c98dc
Mert Demir <info@mertdemir.org>
2023-07-04 15:01:54 +0300

    news app
    For ios and android news app it's not fully ready.
    Includes some features:

---
+++ bin/news.dart
@@ -0,0 +1,5 @@
+import 'package:news/news.dart' as news;
+
+void main(List<String> arguments) {
+  print('Hello world: ${news.calculate()}!');
+}
---
+++ test/news_test.dart
@@ -0,0 +1,8 @@
+import 'package:news/news.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
